82980ad5bbe147229bca1cf5383f16ea28debb40
Alix Carreon <carreonalix@gmail.com>
2020-05-22 16:30:20 -0500

    Agregando Trabajos

---
+++ src/iLoops/DibujarGatitos.java
@@ -0,0 +1,33 @@
+package iLoops;
+
+import libs.Input;
+
+public class DibujarGatitos {
+    public static void main(String[] args) {
+        dibujarPiramide(3);
+        dibujarPiramide(5);
+        dibujarPiramide(10);
+
+    }
+
+    private static void dibujarPiramide(int altura) {
+        for(int i =1; i <= altura; i++) {
+            int numGatitos = i + 1;
+//            int numEspacios =
+            dibujarLinea(2, numGatitos);
+        }
+    }
+
+    private static void dibujarLinea(int numEspacios, int numGatitos) {
+
+        for(int i =0; i < numEspacios; i++) {
+            Input.print(" ");
+        }
+        for(int i =0; i < numGatitos; i++) {
+            Input.print("#");
+        }
+        System.out.println();
+    }
+}
+
+}
